a903fec05f534377fee03e08fb4ed999698284b7
Yuchen Liang <yuchenl3@andrew.cmu.edu>
2023-10-25 19:20:41 -0400

    feat: implement unop, binop, and func expressions
    Signed-off-by: Yuchen Liang <yuchenl3@andrew.cmu.edu>

--- optd-core/src/plan_nodes.rs
+++ optd-core/src/plan_nodes.rs
@@ -8,6 +8,8 @@ use crate::rel_node::{RelNode, RelNodeRef, RelNodeTyp, Value};
 
 pub use filter::LogicalFilter;
 pub use join::{JoinType, LogicalJoin};
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
 pub use scan::LogicalScan;
 
 #[repr(usize)]
@@ -101,6 +103,11 @@ impl ConstantExpr {
             .into(),
         ))
     }
+
+    /// Gets the constant value.
+    pub fn value(&self) -> Value {
+        self.0 .0.data.clone().unwrap()
+    }
 }
 
 impl OptRelNode for ConstantExpr {
@@ -118,10 +125,30 @@ impl OptRelNode for ConstantExpr {
 #[derive(Clone, Debug)]
 pub struct ColumnRefExpr(Expr);
 
+impl ColumnRefExpr {
+    /// Creates a new `ColumnRef` expression.
+    pub fn new(column_idx: usize) -> ColumnRefExpr {
+        ColumnRefExpr(Expr(
+            RelNode {
+                typ: OptRelNodeTyp::ColumnRef,
+                children: vec![],
+                data: Some(Value::Int(column_idx as i64)),
+            }
+            .into(),
+        ))
+    }
+
+    /// Gets the column index.
+    pub fn index(&self) -> usize {
+        usize::from_i64(self.0 .0.data.as_ref().unwrap().as_i64()).unwrap()
+    }
+}
+
 impl OptRelNode for ColumnRefExpr {
     fn into_rel_node(self) -> OptRelNodeRef {
         self.0.into_rel_node()
     }
+
     fn from_rel_node(rel_node: OptRelNodeRef) -> Option<Self> {
         if rel_node.typ != OptRelNodeTyp::ColumnRef {
             return None;
@@ -130,21 +157,154 @@ impl OptRelNode for ColumnRefExpr {
     }
 }
 
-pub fn column_ref(column_idx: usize) -> ConstantExpr {
-    ConstantExpr(Expr(
-        RelNode {
-            typ: OptRelNodeTyp::ColumnRef,
-            children: vec![],
-            data: Some(Value::Int(column_idx as i64)),
+#[derive(FromPrimitive)]
+pub enum UnOpType {
+    Neg = 1,
+    Not,
+}
+
+#[derive(Clone, Debug)]
+pub struct UnOpExpr(Expr);
+
+impl UnOpExpr {
+    pub fn new(child: Expr, op_type: UnOpType) -> Self {
+        UnOpExpr(Expr(
+            RelNode {
+                typ: OptRelNodeTyp::UnOp,
+                children: vec![child.into_rel_node()],
+                data: Some(Value::Int(op_type as i64)),
+            }
+            .into(),
+        ))
+    }
+
+    pub fn child(&self) -> Expr {
+        Expr::from_rel_node(self.clone().into_rel_node().children[0].clone()).unwrap()
+    }
+
+    pub fn op_type(&self) -> UnOpType {
+        UnOpType::from_i64(self.0 .0.data.as_ref().unwrap().as_i64()).unwrap()
+    }
+}
+
+impl OptRelNode for UnOpExpr {
+    fn into_rel_node(self) -> OptRelNodeRef {
+        self.0.into_rel_node()
+    }
+
+    fn from_rel_node(rel_node: OptRelNodeRef) -> Option<Self> {
+        if rel_node.typ != OptRelNodeTyp::UnOp {
+            return None;
         }
-        .into(),
-    ))
+        Expr::from_rel_node(rel_node).map(Self)
+    }
+}
+
+#[derive(FromPrimitive)]
+pub enum BinOpType {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    Eq,
+    Neq,
+    Gt,
+    Lt,
+    Geq,
+    Leq,
+    And,
+    Or,
+    Xor,
+}
+
+#[derive(Clone, Debug)]
+pub struct BinOpExpr(Expr);
+
+impl BinOpExpr {
+    pub fn new(left: Expr, right: Expr, op_type: BinOpType) -> Self {
+        BinOpExpr(Expr(
+            RelNode {
+                typ: OptRelNodeTyp::BinOp,
+                children: vec![left.into_rel_node(), right.into_rel_node()],
+                data: Some(Value::Int(op_type as i64)),
+            }
+            .into(),
+        ))
+    }
+
+    pub fn left_child(&self) -> Expr {
+        Expr::from_rel_node(self.clone().into_rel_node().children[0].clone()).unwrap()
+    }
+
+    pub fn right_child(&self) -> Expr {
+        Expr::from_rel_node(self.clone().into_rel_node().children[1].clone()).unwrap()
+    }
+
+    pub fn op_type(&self) -> BinOpType {
+        BinOpType::from_i64(self.0 .0.data.as_ref().unwrap().as_i64()).unwrap()
+    }
+}
+
+impl OptRelNode for BinOpExpr {
+    fn into_rel_node(self) -> OptRelNodeRef {
+        self.0.into_rel_node()
+    }
+
+    fn from_rel_node(rel_node: OptRelNodeRef) -> Option<Self> {
+        if rel_node.typ != OptRelNodeTyp::BinOp {
+            return None;
+        }
+        Expr::from_rel_node(rel_node).map(Self)
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct FuncExpr(Expr);
+
+impl FuncExpr {
+    pub fn new(func_id: usize, argv: Vec<Expr>) -> Self {
+        FuncExpr(Expr(
+            RelNode {
+                typ: OptRelNodeTyp::Func,
+                children: argv.into_iter().map(Expr::into_rel_node).collect(),
+                data: Some(Value::Int(func_id as i64)),
+            }
+            .into(),
+        ))
+    }
+
+    /// Gets the i-th argument of the function.
+    pub fn arg_at(&self, i: usize) -> Expr {
+        Expr::from_rel_node(self.clone().into_rel_node().children[i].clone()).unwrap()
+    }
+
+    /// Gets the function id.
+    pub fn id(&self) -> usize {
+        usize::from_i64(self.0 .0.data.as_ref().unwrap().as_i64()).unwrap()
+    }
+}
+
+impl OptRelNode for FuncExpr {
+    fn into_rel_node(self) -> OptRelNodeRef {
+        self.0.into_rel_node()
+    }
+
+    fn from_rel_node(rel_node: OptRelNodeRef) -> Option<Self> {
+        if rel_node.typ != OptRelNodeTyp::Func {
+            return None;
+        }
+        Expr::from_rel_node(rel_node).map(Self)
+    }
 }
 
 pub fn explain(rel_node: OptRelNodeRef) {
     match rel_node.typ {
         OptRelNodeTyp::ColumnRef => ColumnRefExpr::from_rel_node(rel_node).unwrap().explain(),
         OptRelNodeTyp::Constant => ConstantExpr::from_rel_node(rel_node).unwrap().explain(),
+        OptRelNodeTyp::UnOp => BinOpExpr::from_rel_node(rel_node).unwrap().explain(),
+        OptRelNodeTyp::BinOp => BinOpExpr::from_rel_node(rel_node).unwrap().explain(),
+        OptRelNodeTyp::Func => BinOpExpr::from_rel_node(rel_node).unwrap().explain(),
         OptRelNodeTyp::Join => LogicalJoin::from_rel_node(rel_node).unwrap().explain(),
         OptRelNodeTyp::Scan => LogicalScan::from_rel_node(rel_node).unwrap().explain(),
         OptRelNodeTyp::Filter => LogicalFilter::from_rel_node(rel_node).unwrap().explain(),
