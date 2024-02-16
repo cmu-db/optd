87d3435a44605a96870e157c81902040c2ddc6fb
Patrick Wang <wang.patrick57@gmail.com>
2024-02-16 15:59:44 -0500

    ci.sh

---
+++ ci.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# runs the stuff in CI.yaml locally
+# unfortunately this needs to be updated manually. just update it if you get annoyed by GHAs failing
+cargo fmt --all -- --check
+cargo clippy --workspace --all-targets --all-features --locked -- -D warnings
+cargo test --no-fail-fast --workspace --all-features --locked
