e0dd74ad5ddb00adfd2c10214225d9a45256a7ec
Alexis Schlomer <aschlome@andrew.cmu.edu>
2024-03-06 00:58:32 -0500

    Add merge into misra gries

--- optd-gungnir/src/stats/misragries.rs
+++ optd-gungnir/src/stats/misragries.rs
@@ -7,9 +7,9 @@ use std::{collections::HashMap, hash::Hash};
 
 /// The Misra-Gries structure to approximate the k most frequent elements in
 /// a stream of N elements. It will always identify elements with frequency
-/// f > n/k, and include additional leftovers.
+/// f >= (n/k), and include additional leftovers.
 pub struct MisraGries<T: PartialEq + Eq + Hash + Clone> {
-    frequencies: HashMap<T, u32>, // The approximated frequencies of an element T.
+    frequencies: HashMap<T, i32>, // The approximated frequencies of an element T.
     k: u16,                       // The max size of our frequencies hashmap.
 }
 
@@ -20,38 +20,64 @@ where
 {
     /// Creates and initializes a new empty Misra-Gries.
     pub fn new(k: u16) -> Self {
+        assert!(k > 0);
+
         MisraGries::<T> {
             frequencies: HashMap::with_capacity(k as usize),
             k,
         }
     }
 
-    /// Digests an array of data into the Misra-Gries structure.
-    pub fn aggregate(&mut self, data: &[T]) {
-        for elem in data {
-            match self.frequencies.get_mut(elem) {
-                Some(freq) => *freq += 1,
-                None => {
-                    if self.frequencies.len() < self.k as usize {
-                        self.frequencies.insert(elem.clone(), 1);
-                    } else {
-                        for freq in self.frequencies.values_mut() {
-                            *freq -= 1;
-                        }
+    // Inserts an element occ times into the `self` Misra-Gries structure.
+    fn insert_element(&mut self, elem: T, occ: i32) {
+        match self.frequencies.get_mut(&elem) {
+            Some(freq) => *freq += occ,
+            None => {
+                if self.frequencies.len() < self.k as usize {
+                    self.frequencies.insert(elem, occ);
+                } else {
+                    for freq in self.frequencies.values_mut() {
+                        *freq -= occ;
+                    }
 
-                        let keys_to_remove: Vec<T> = self
-                            .frequencies
-                            .iter()
-                            .filter(|&(_, &freq)| freq <= 0)
-                            .map(|(key, _)| key.clone())
-                            .collect();
+                    let smallest_freq = self.frequencies.values().min().copied().unwrap();
+                    let keys_to_remove: Vec<T> = self
+                        .frequencies
+                        .iter()
+                        .filter(|&(_, &freq)| freq <= 0)
+                        .map(|(key, _)| key.clone())
+                        .collect();
 
-                        for key in keys_to_remove {
-                            self.frequencies.remove(&key);
-                        }
+                    for key in keys_to_remove {
+                        self.frequencies.remove(&key);
+                    }
+
+                    if smallest_freq < 0 {
+                        self.frequencies.insert(elem, -smallest_freq);
                     }
                 }
             }
         }
     }
+
+    /// Digests an array of data into the Misra-Gries structure.
+    pub fn aggregate(&mut self, data: &[T]) {
+        data.iter()
+            .for_each(|key| self.insert_element(key.clone(), 1));
+    }
+
+    /// Merges another MisraGries into the current one.
+    /// Particularly useful for parallel execution.
+    pub fn merge(&mut self, other: &MisraGries<T>) {
+        assert!(self.k == other.k);
+
+        other
+            .frequencies
+            .iter()
+            .for_each(|(key, occ)| self.insert_element(key.clone(), *occ));
+    }
 }
+
+// Start of unit testing section.
+#[cfg(test)]
+mod tests {}
