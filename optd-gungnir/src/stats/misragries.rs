2e181356fef89d83c6d73af0a1dd349d67cb2737
Alexis Schlomer <aschlome@andrew.cmu.edu>
2024-03-06 14:04:34 -0500

    Refine Misra Gries MFV estimate

--- optd-gungnir/src/stats/misragries.rs
+++ optd-gungnir/src/stats/misragries.rs
@@ -1,17 +1,18 @@
 //! Implementation of the Misra-Gries Summary data structure as described in  
 //! the Cormode et al. paper: "Misra-Gries Summaries" (2014).
+//! We further refine the algorithm to ensure that K elements will always be
+//! returned, as long as K <= N.
 //! For more details, refer to:
 //! https://people.csail.mit.edu/rrw/6.045-2017/encalgs-mg.pdf
 
-use std::{collections::HashMap, fmt::Debug, hash::Hash};
+use std::{cmp::min, collections::HashMap, hash::Hash};
 
 use itertools::Itertools;
 
 /// The Misra-Gries structure to approximate the k most frequent elements in
 /// a stream of N elements. It will always identify elements with frequency
 /// f >= (n/k), and include additional leftovers.
-#[derive(Debug)]
-pub struct MisraGries<T: PartialEq + Eq + Hash + Clone + Debug> {
+pub struct MisraGries<T: PartialEq + Eq + Hash + Clone> {
     frequencies: HashMap<T, i32>, // The approximated frequencies of an element T.
     k: u16,                       // The max size of our frequencies hashmap.
 }
@@ -19,7 +20,7 @@ pub struct MisraGries<T: PartialEq + Eq + Hash + Clone + Debug> {
 // Self-contained implementation of the Misra-Gries data structure.
 impl<T> MisraGries<T>
 where
-    T: PartialEq + Eq + Hash + Clone + Debug,
+    T: PartialEq + Eq + Hash + Clone,
 {
     /// Creates and initializes a new empty Misra-Gries.
     pub fn new(k: u16) -> Self {
@@ -31,32 +32,36 @@ where
         }
     }
 
+    // Returns the (key, val) pair of the least frequent element.
+    // If more than one such element exists, returns an arbitrary one.
+    // NOTE: Panics if no frequencies exist.
+    fn find_least_frequent(&self) -> (T, i32) {
+        let (key, occ) = self.frequencies.iter().min_by_key(|(_, occ)| *occ).unwrap();
+        (key.clone(), *occ)
+    }
+
     // Inserts an element occ times into the `self` Misra-Gries structure.
     fn insert_element(&mut self, elem: T, occ: i32) {
         match self.frequencies.get_mut(&elem) {
-            Some(freq) => *freq += occ,
+            Some(freq) => *freq += occ, // Hit.
             None => {
                 if self.frequencies.len() < self.k as usize {
-                    self.frequencies.insert(elem, occ);
+                    self.frequencies.insert(elem, occ); // Discovery phase.
                 } else {
-                    for freq in self.frequencies.values_mut() {
-                        *freq -= occ;
+                    // Check if there *will* be an evictable frequency; decrement & insert.
+                    let (smallest_freq_key, smallest_freq) = self.find_least_frequent();
+
+                    let decr = min(smallest_freq, occ);
+                    if decr > 0 {
+                        for freq in self.frequencies.values_mut() {
+                            *freq -= decr;
+                        }
                     }
 
-                    let smallest_freq = self.frequencies.values().min().copied().unwrap();
-                    let keys_to_remove: Vec<T> = self
-                        .frequencies
-                        .iter()
-                        .filter(|&(_, &freq)| freq <= 0)
-                        .map(|(key, _)| key.clone())
-                        .collect();
-
-                    for key in keys_to_remove {
-                        self.frequencies.remove(&key);
-                    }
-
-                    if smallest_freq < 0 {
-                        self.frequencies.insert(elem, -smallest_freq);
+                    let delta = smallest_freq - occ;
+                    if delta < 0 {
+                        self.frequencies.remove(&smallest_freq_key);
+                        self.frequencies.insert(elem, -delta);
                     }
                 }
             }
@@ -80,9 +85,8 @@ where
             .for_each(|(key, occ)| self.insert_element(key.clone(), *occ));
     }
 
-    /// Returns an estimate of the K most frequent keys.
-    /// Returns less than K results if less than N distinct
-    /// values have been encountered so far.
+    /// Returns all elements with frequency f >= (n/k),
+    /// and may include additional leftovers.
     pub fn most_frequent_keys(&self) -> Vec<&T> {
         self.frequencies.keys().collect_vec()
     }
@@ -123,26 +127,28 @@ mod tests {
 
     #[test]
     fn aggregate_zipfian() {
-        // TODO(Alexis): Cleanup below & handle only 4 but have room for 16.
-        let n_distinct = 1000;
-        let mut data = Vec::<i32>::new();
+        let n_distinct = 10000;
+        let k = 200;
 
+        let mut data = Vec::<i32>::new();
         for idx in 1..=n_distinct {
             let occurance = n_distinct / idx;
             for _ in 0..occurance {
                 data.push(idx);
             }
         }
-
         let mut rng = StdRng::seed_from_u64(0);
         data.shuffle(&mut rng);
 
-        let n = data.len();
-        let mut misra_gries = MisraGries::<i32>::new(16);
+        let n = data.len() as i32;
+        let mut misra_gries = MisraGries::<i32>::new(k as u16);
 
         misra_gries.aggregate(&data);
 
-        println!("{:#?}", misra_gries);
-        println!("{}", n);
+        let mfk = misra_gries.most_frequent_keys();
+        assert!((1..=n_distinct)
+            .filter(|idx| (n_distinct / idx) * k >= n)
+            .all(|idx| mfk.contains(&&idx)));
+        assert!(mfk.len() == (k as usize));
     }
 }
