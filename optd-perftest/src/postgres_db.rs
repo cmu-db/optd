a0b2bb07ada126429a3f96696e911934a943ace0
Patrick Wang <wang.patrick57@gmail.com>
2024-03-08 15:17:06 -0800

    PostgresDb now uses instance methods for its build functions

--- optd-perftest/src/postgres_db.rs
+++ optd-perftest/src/postgres_db.rs
@@ -28,27 +28,28 @@ impl PostgresDb {
         }
         let pgdata_dpath = postgres_db_dpath.join("pgdata");
 
-        // (re)start postgres
-        // TODO(phw2): do this in next commit
-        PostgresDb::install_postgres(verbose).await?;
-        PostgresDb::init_pgdata(&pgdata_dpath, verbose).await?;
-
         // create Self
         let db = PostgresDb {verbose, postgres_db_dpath, pgdata_dpath};
+
+        // (re)start postgres
+        // TODO(phw2): do this in next commit
+        db.install_postgres().await?;
+        db.init_pgdata().await?;
+
         Ok(db)
     }
 
     /// Installs an up-to-date version of Postgres using the OS's package manager
-    async fn install_postgres(verbose: bool) -> Result<()> {
+    async fn install_postgres(&self) -> Result<()> {
         match OS {
             "macos" => {
-                if verbose {
+                if self.verbose {
                     println!("updating and upgrading brew...");
                 }
                 shell::run_command_with_status_check("brew update")?;
                 shell::run_command_with_status_check("brew upgrade")?;
 
-                if verbose {
+                if self.verbose {
                     println!("installing postgresql...");
                 }
                 shell::run_command_with_status_check("brew install postgresql")?;
@@ -59,21 +60,18 @@ impl PostgresDb {
     }
 
     /// Initializes pgdata_dpath directory if it wasn't already initialized
-    async fn init_pgdata<P>(pgdata_dpath: P, verbose: bool) -> Result<()>
-    where
-        P: AsRef<Path>,
-    {
-        let done_fpath = pgdata_dpath.as_ref().join("initdb_done");
+    async fn init_pgdata(&self) -> Result<()> {
+        let done_fpath = self.pgdata_dpath.join("initdb_done");
         if !done_fpath.exists() {
-            if verbose {
+            if self.verbose {
                 println!("running initdb...");
             }
-            shell::make_into_empty_dir(&pgdata_dpath)?;
-            shell::run_command_with_status_check(&format!("initdb {}", pgdata_dpath.as_ref().to_str().unwrap()))?;
+            shell::make_into_empty_dir(&self.pgdata_dpath)?;
+            shell::run_command_with_status_check(&format!("initdb {}", self.pgdata_dpath.to_str().unwrap()))?;
             File::create(done_fpath)?;
         } else {
             #[allow(clippy::collapsible_else_if)]
-            if verbose {
+            if self.verbose {
                 println!(
                     "skipped running initdb"
                 );
@@ -86,10 +84,7 @@ impl PostgresDb {
     /// As an optimization, if this benchmark only has read-only queries and the
     ///   data currently loaded was with the same benchmark and parameters, we don't
     ///   need to load it again
-    pub async fn load_benchmark_data<P>(pgdata_dpath: P, verbose: bool) -> Result<()>
-    where
-        P: AsRef<Path>,
-    {
+    pub async fn load_benchmark_data(&self) -> Result<()> {
         shell::run_command_with_status_check(&format!("create db {}", OPTD_DB_NAME))?;
         Ok(())
     }
