0427fc0186821786a157f9ffa76b02580e73b540
Alexis Schlomer <aschlome@andrew.cmu.edu>
2024-03-08 14:09:11 -0500

    Finish Migra Gries testing

--- optd-gungnir/src/stats/misragries.rs
+++ optd-gungnir/src/stats/misragries.rs
@@ -95,7 +95,11 @@ where
 // Start of unit testing section.
 #[cfg(test)]
 mod tests {
+    use std::sync::atomic::{AtomicUsize, Ordering};
+    use std::sync::{Arc, Mutex};
+
     use super::MisraGries;
+    use crossbeam::thread;
     use rand::seq::SliceRandom;
     use rand::{rngs::StdRng, SeedableRng};
 
@@ -125,11 +129,9 @@ mod tests {
         }
     }
 
-    #[test]
-    fn aggregate_zipfian() {
-        let n_distinct = 10000;
-        let k = 200;
-
+    // Generates a shuffled array of n distinct elements following a Zipfian
+    // distribution based on the provided seed.
+    fn create_zipfian(n_distinct: i32, seed: u64) -> Vec<i32> {
         let mut data = Vec::<i32>::new();
         for idx in 1..=n_distinct {
             let occurance = n_distinct / idx;
@@ -137,18 +139,64 @@ mod tests {
                 data.push(idx);
             }
         }
-        let mut rng = StdRng::seed_from_u64(0);
+        let mut rng = StdRng::seed_from_u64(seed);
         data.shuffle(&mut rng);
 
-        let n = data.len() as i32;
+        data
+    }
+
+    // Verifies the ability of Misra-Gries in identifying the most frequent elements
+    // in a dataset following a Zipfian distribution.
+    fn check_zipfian(misra_gries: &MisraGries<i32>, n_distinct: i32) {
+        let mfk = misra_gries.most_frequent_keys();
+        let k = misra_gries.k as i32;
+        let total_length: i32 = (1..=n_distinct).map(|idx| n_distinct / idx).sum();
+
+        assert!((1..=n_distinct)
+            .filter(|idx| (n_distinct / idx) * k >= total_length)
+            .all(|idx| mfk.contains(&&idx)));
+        assert!(mfk.len() == (k as usize));
+    }
+
+    #[test]
+    fn aggregate_zipfian() {
+        let n_distinct = 10000;
+        let k = 200;
+
+        let data = create_zipfian(n_distinct, 0);
         let mut misra_gries = MisraGries::<i32>::new(k as u16);
 
         misra_gries.aggregate(&data);
 
-        let mfk = misra_gries.most_frequent_keys();
-        assert!((1..=n_distinct)
-            .filter(|idx| (n_distinct / idx) * k >= n)
-            .all(|idx| mfk.contains(&&idx)));
-        assert!(mfk.len() == (k as usize));
+        check_zipfian(&misra_gries, n_distinct);
+    }
+
+    #[test]
+    fn merge_zipfians() {
+        let n_distinct = 10000;
+        let n_jobs = 16;
+        let k = 200;
+
+        let result_misra_gries = Arc::new(Mutex::new(MisraGries::<i32>::new(k as u16)));
+        let job_id = AtomicUsize::new(0);
+        thread::scope(|s| {
+            for _ in 0..n_jobs {
+                s.spawn(|_| {
+                    let mut local_misra_gries = MisraGries::<i32>::new(k as u16);
+                    let curr_job_id = job_id.fetch_add(1, Ordering::SeqCst);
+
+                    let data = create_zipfian(n_distinct, curr_job_id as u64);
+                    local_misra_gries.aggregate(&data);
+
+                    check_zipfian(&local_misra_gries, n_distinct);
+
+                    let mut result = result_misra_gries.lock().unwrap();
+                    result.merge(&local_misra_gries);
+                });
+            }
+        })
+        .unwrap();
+
+        check_zipfian(&result_misra_gries.lock().unwrap(), n_distinct);
     }
 }
