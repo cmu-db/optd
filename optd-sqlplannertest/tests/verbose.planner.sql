06ee4050b4cd055e17e1ef012cdb59daa5462cea
Gun9niR <gun9nir.guo@gmail.com>
2024-03-13 12:14:54 -0400

    display more fields

--- optd-sqlplannertest/tests/verbose.planner.sql
+++ optd-sqlplannertest/tests/verbose.planner.sql
@@ -18,20 +18,20 @@ PhysicalProjection { exprs: [ #0 ] }
 select * from t1;
 
 /*
-PhysicalProjection { exprs: [ #0 ], rows: 1 }
-└── PhysicalScan { table: t1, rows: 1 }
+PhysicalProjection { exprs: [ #0 ], cost: weighted=1.06,row_cnt=1.00,compute=0.06,io=1.00 }
+└── PhysicalScan { table: t1, cost: weighted=1.00,row_cnt=1.00,compute=0.00,io=1.00 }
 */
 
 -- Test verbose explain with aggregation
 select count(*) from t1;
 
 /*
-PhysicalProjection { exprs: [ #0 ], rows: 1 }
+PhysicalProjection { exprs: [ #0 ], cost: weighted=21.18,row_cnt=1.00,compute=20.18,io=1.00 }
 └── PhysicalAgg
     ├── aggrs:Agg(Count)
     │   └── [ 1 ]
     ├── groups: []
-    ├── rows: 1
-    └── PhysicalScan { table: t1, rows: 1 }
+    ├── cost: weighted=21.12,row_cnt=1.00,compute=20.12,io=1.00
+    └── PhysicalScan { table: t1, cost: weighted=1.00,row_cnt=1.00,compute=0.00,io=1.00 }
 */
 
