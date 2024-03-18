967d6a9b0b8e12645781f359e4c727083005213a
Patrick Wang <wang.patrick57@gmail.com>
2024-03-18 12:51:40 -0400

    more fields in truecard cache

--- optd-perftest/src/truecard_cache.rs
+++ optd-perftest/src/truecard_cache.rs
@@ -1,7 +1,13 @@
+/// A cache that gets persisted to disk for the true cardinalities of queries on a DBMS
 struct TruecardCache {
-    cache: HashMap<String, usize>,
+    // The DBMS the queries of this cache were executed on
+    dbms_name: String,
+    // The cache from dbname -> sql -> cardinality
+    // Note that dbname is the database _within_ the DBMS that sql was executed on
+    cache: HashMap<String, HashMap<String, usize>>,
 }
 
+// TODO(phw2): write function that takes in a Benchmark and creates a new entry in the cache
 impl TruecardCache {
     
 }
