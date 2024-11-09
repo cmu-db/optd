2113cb1018b4ecdd46a1486dcb0d4c76841a6f07
Alex Chi <iskyzh@gmail.com>
2024-11-09 12:25:47 -0500

    license
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- optd-core/src/tests.rs
+++ optd-core/src/tests.rs
@@ -1,2 +1,7 @@
+// Copyright (c) 2023-2024 CMU Database Group
+//
+// Use of this source code is governed by an MIT-style license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
 pub(crate) mod common;
 pub(crate) mod heuristics_physical_property;
