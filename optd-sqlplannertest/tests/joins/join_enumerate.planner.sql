4b90d28294c557e2c585ba442accb2ad5c3b7086
Alex Chi Z <iskyzh@gmail.com>
2024-12-16 23:30:31 -0500

    test: reproduce hash join planning issue (#265)
    Signed-off-by: Alex Chi Z <iskyzh@gmail.com>

--- optd-sqlplannertest/tests/joins/join_enumerate.planner.sql
+++ optd-sqlplannertest/tests/joins/join_enumerate.planner.sql
@@ -82,27 +82,35 @@ select * from t2, t1, t3 where t1v1 = t2v1 and t1v2 = t3v2;
 */
 
 -- Test whether the optimizer enumerates all 3-join orders. (It don't currently)
-select * from t1, t2, t3 where t1v1 = t2v1 and t1v2 = t3v2;
+select * from t1, (select * from t2, t3) where t1v1 = t2v1 and t1v2 = t3v2;
 
 /*
-(Join t2 (Join t1 t3))
-(Join t2 (Join t3 t1))
-(Join t3 (Join t1 t2))
-(Join t3 (Join t2 t1))
-(Join (Join t1 t2) t3)
-(Join (Join t1 t3) t2)
-(Join (Join t2 t1) t3)
-(Join (Join t3 t1) t2)
+(Join t1 (Join t2 t3))
+(Join (Join t2 t3) t1)
 
-(Join t2 (Join t1 t3))
-(Join t2 (Join t3 t1))
-(Join t3 (Join t1 t2))
-(Join t3 (Join t2 t1))
-(Join (Join t1 t2) t3)
-(Join (Join t1 t3) t2)
-(Join (Join t2 t1) t3)
-(Join (Join t3 t1) t2)
+(Join t1 (Join t2 t3))
+(Join (Join t2 t3) t1)
 
+LogicalProjection { exprs: [ #0, #1, #2, #3, #4, #5 ] }
+└── LogicalFilter
+    ├── cond:And
+    │   ├── Eq
+    │   │   ├── #0
+    │   │   └── #2
+    │   └── Eq
+    │       ├── #1
+    │       └── #4
+    └── LogicalJoin { join_type: Cross, cond: true }
+        ├── LogicalScan { table: t1 }
+        └── LogicalProjection { exprs: [ #0, #1, #2, #3 ] }
+            └── LogicalJoin { join_type: Cross, cond: true }
+                ├── LogicalScan { table: t2 }
+                └── LogicalScan { table: t3 }
+PhysicalHashJoin { join_type: Inner, left_keys: [ #0, #1 ], right_keys: [ #0, #2 ] }
+├── PhysicalScan { table: t1 }
+└── PhysicalNestedLoopJoin { join_type: Cross, cond: true }
+    ├── PhysicalScan { table: t2 }
+    └── PhysicalScan { table: t3 }
 0 0 0 200 0 300
 1 1 1 201 1 301
 2 2 2 202 2 302
