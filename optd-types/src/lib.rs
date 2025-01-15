1a8be917fb7ba97f472fb29c02d8190d5346a2a2
Connor Tsui <connor.tsui20@gmail.com>
2025-01-14 21:47:48 -0500

    first draft of attempt at modeling optimization with persistent memo

--- optd-types/src/lib.rs
+++ optd-types/src/lib.rs
@@ -1,14 +1,37 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+mod memo;
+pub use memo::MemoNode;
+
+/// A type representing a transformation or implementation rule for query operators.
+///
+/// TODO The variants are just placeholders.
+pub enum Rule {
+    Transformation,
+    Implementation
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct GroupId(usize);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+/// An in-memory tree of logical operators. Used as the input / entrypoint of the optimizer.
+///
+/// TODO The variants are just placeholders. The actual type will look more similar to
+/// https://docs.rs/substrait/latest/substrait/proto/rel/enum.RelType.html
+pub enum LogicalPlan {
+    Scan(String),
+    Filter(Box<LogicalPlan>),
+    Sort(Box<LogicalPlan>),
+    Join(Box<LogicalPlan>),
+}
+
+/// An in-memory tree of physical operators. Used as the final materialized output of the optimizer.
+///
+/// TODO The variants are just placeholders. The actual type will look more similar to
+/// https://docs.rs/substrait/latest/substrait/proto/rel/enum.RelType.html
+pub enum PhysicalPlan {
+    TableScan(String),
+    IndexScan(String),
+    PhysicalFilter(Box<PhysicalPlan>),
+    NestedLoopJoin(Box<PhysicalPlan>),
+    SortMergeJoin(Box<PhysicalPlan>),
+    HashJoin(Box<PhysicalPlan>),
+    MergeSort(Box<PhysicalPlan>),
 }
