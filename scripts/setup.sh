80076683dc6bf6e50aaab9e7ba4c770abbf1384b
Yuchen Liang <yuchenl3@andrew.cmu.edu>
2025-01-21 10:38:21 -0500

    bundle libsqlite3-sys to avoid build issues
    Signed-off-by: Yuchen Liang <yuchenl3@andrew.cmu.edu>

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,6 @@
-# cargo-binstall enables directly installing Rust binaries.
-cargo install cargo-binstall && 
 # diesel_cli is a CLI tool for managing database schema, runnig migrations, 
 # and generating code. when working with Diesel (https://diesel.rs/).
-cargo binstall diesel_cli &&
+cargo install diesel_cli --no-default-features --features sqlite-bundled &&
 # Setup a new database and runs the migrations.
 echo DATABASE_URL=test_memo.db > .env &&
 diesel setup
