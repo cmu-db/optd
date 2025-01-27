33a026b5b604e389c7b71de86a6b0ad2580bccc7
SarveshOO7 <sarveshtandonus@gmail.com>
2025-01-27 18:35:49 -0500

    Moved from crate to submodules

--- infra/src/lib.rs
+++ infra/src/lib.rs
@@ -1,8 +1,25 @@
 #![doc = include_str!("../../README.md")]
+#![allow(unused)]
+
 
 use datafusion::common::Result;
 use datafusion::execution::context::SessionContext;
 use datafusion::prelude::*;
+
+
+pub mod expression;
+pub mod memo;
+pub mod operator;
+pub mod plan;
+
+/// TODO make distinction between relational groups and scalar groups.
+#[repr(transparent)]
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct GroupId(u64);
+
+/// TODO Add docs.
+pub struct ExprId(u64);
+
 
 mod query_planner;
 
