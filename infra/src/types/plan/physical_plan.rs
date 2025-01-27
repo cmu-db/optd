469b36cc39fee68d6253664b8b9a4ac523104119
Connor Tsui <connor.tsui20@gmail.com>
2025-01-27 18:55:07 -0500

    fix plan types

--- infra/src/types/plan/physical_plan.rs
+++ infra/src/types/plan/physical_plan.rs
@@ -1,20 +1,12 @@
-use crate::types::operator::{logical::LogicalOperator, physical::PhysicalOperator, ScalarOperator};
+use crate::types::operator::{
+    logical::LogicalOperator, physical::PhysicalOperator, ScalarOperator,
+};
 use std::sync::Arc;
 
 /// TODO Add docs.
 #[derive(Clone)]
-pub enum PhysicalPlan {
-    LogicalRoot(LogicalLink),
-    PhysicalRoot(PhysicalLink),
-}
-
-/// TODO Add docs.
-#[allow(clippy::enum_variant_names)]
-#[derive(Clone)]
-pub enum LogicalLink {
-    LogicalNode(Arc<LogicalOperator<LogicalLink>>),
-    PhysicalNode(Arc<PhysicalOperator<PhysicalLink>>),
-    ScalarNode(Arc<ScalarOperator<ScalarLink>>),
+pub struct PhysicalPlan {
+    root: Arc<PhysicalOperator<PhysicalLink>>,
 }
 
 /// TODO Add docs.
