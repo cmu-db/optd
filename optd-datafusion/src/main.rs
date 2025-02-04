0f6e8382e3203f33468a60fa76b06c9411eda953
SarveshOO7 <sarveshtandonus@gmail.com>
2025-02-03 19:50:22 -0500

    Filters do not work right now because Expr::Cast has not been implemented yet

--- optd-datafusion/src/main.rs
+++ optd-datafusion/src/main.rs
@@ -16,8 +16,17 @@ mod tests {
     use optd_datafusion::run_queries;
 
     #[tokio::test]
-    async fn test_simple() {
-        let mut file = std::fs::File::open("sql/test1.sql").expect("Failed to open file");
+    async fn test_scan() {
+        let mut file = std::fs::File::open("sql/test_scan.sql").expect("Failed to open file");
+        let mut queries = String::new();
+        file.read_to_string(&mut queries)
+            .expect("Failed to read from file");
+        run_queries(queries).await.expect("Failed to run queries");
+    }
+
+    #[tokio::test]
+    async fn test_filter() {
+        let mut file = std::fs::File::open("sql/test_filter.sql").expect("Failed to open file");
         let mut queries = String::new();
         file.read_to_string(&mut queries)
             .expect("Failed to read from file");
