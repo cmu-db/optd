2cbe4ea0b083538ec5f2b227438750c56b6c5c3b
Yuchen Liang <yuchenl3@andrew.cmu.edu>
2025-02-18 13:53:40 -0500

    add initial catalog
    Signed-off-by: Yuchen Liang <yuchenl3@andrew.cmu.edu>

--- optd-core/src/storage/migrations/20250122134426_create_table_metadata.up.sql
+++ optd-core/src/storage/migrations/20250122134426_create_table_metadata.up.sql
@@ -1,5 +1,8 @@
 CREATE TABLE table_metadata (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    -- The name of the table,
+    name TEXT NOT NULL,
+    -- The namespace that the table belongs to.
     namespace_id BIGINT NOT NULL,
     -- The time at which the database was created.
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
