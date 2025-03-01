444b953c99e330f067fbc22a41eeeb77f1fe09c9
SarveshOO7 <sarveshtandonus@gmail.com>
2025-03-01 12:12:26 -0500

    added more properties

--- optd-core/src/ir/properties.rs
+++ optd-core/src/ir/properties.rs
@@ -1,19 +1,25 @@
+use ordered_float::OrderedFloat;
 use serde::{Deserialize, Serialize};
 
 use super::plans::ScalarPlan;
 
-#[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
-pub struct PhysicalProperties {
-    pub data: Vec<PropertyData>,
-}
-
-#[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
 pub struct LogicalProperties {
     data: Vec<PropertyData>,
 }
 
-#[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
+pub struct PhysicalProperties {
+    pub data: Vec<PropertyData>,
+}
+
+#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
 pub enum PropertyData {
     Int64(i64),
+    Float64(OrderedFloat<f64>),
+    String(String),
+    Bool(bool),
+    Struct(String, Vec<PropertyData>),
+    Array(Vec<PropertyData>),
     Scalar(ScalarPlan),
 }
