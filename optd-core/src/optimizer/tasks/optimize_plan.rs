d8f9d6404a6a3b684c38f75035393b29d707de02
Yuchen Liang <yuchenl3@andrew.cmu.edu>
2025-04-04 17:03:16 -0400

    implement create_optimize_plan_task
    Signed-off-by: Yuchen Liang <yuchenl3@andrew.cmu.edu>

--- optd-core/src/optimizer/tasks/optimize_plan.rs
+++ optd-core/src/optimizer/tasks/optimize_plan.rs
@@ -1,12 +1,13 @@
-use futures::channel::mpsc;
+use futures::{SinkExt, channel::mpsc};
 
 use crate::{
-    cir::{GoalId, LogicalPlan, PhysicalPlan},
+    cir::{Goal, GoalId, LogicalPlan, PhysicalPlan, PhysicalProperties},
+    error::Error,
     memo::Memoize,
-    optimizer::Optimizer,
+    optimizer::{Optimizer, tasks::SourceTaskId},
 };
 
-use super::TaskId;
+use super::{Task, TaskId};
 
 /// Task data for optimizing a logical plan.
 #[derive(Debug)]
@@ -33,6 +34,13 @@ impl OptimizePlanTask {
             optimize_goal_in,
         }
     }
+
+    pub fn send_best_plan(&self, plan: PhysicalPlan) {
+        let mut tx = self.response_tx.clone();
+        tokio::spawn(async move {
+            tx.send(plan).await.unwrap();
+        });
+    }
 }
 
 impl<M: Memoize> Optimizer<M> {
@@ -40,13 +48,28 @@ impl<M: Memoize> Optimizer<M> {
         &mut self,
         plan: LogicalPlan,
         response_tx: mpsc::Sender<PhysicalPlan>,
-    ) -> TaskId {
-        // Do ingestion
-        // logical_expresison, group_id
-        // goal_id = group + empty prop create if not exists
-        // ensure_goal_optimize exists, create subtasks
-        // return
+    ) -> Result<TaskId, Error> {
+        let task_id = self.next_task_id();
 
-        todo!()
+        let group_id = self.ingest_logical_plan(&plan.clone().into()).await?;
+        let goal_id = self
+            .memo
+            .get_goal_id(&Goal(group_id, PhysicalProperties(None)))
+            .await?;
+
+        let (optimize_goal_in, best_costed) = self
+            .ensure_optimize_goal_task(goal_id, SourceTaskId::OptimizePlan(task_id))
+            .await?;
+
+        let task = OptimizePlanTask::new(plan, response_tx, optimize_goal_in);
+
+        if let Some((physical_expr_id, _)) = best_costed {
+            // TODO: return value of egest_best_plan is always `Some(plan)` because we already have it costed.
+            let physical_plan = self.egest_best_plan(physical_expr_id).await?.unwrap();
+            task.send_best_plan(physical_plan);
+        }
+
+        self.tasks.insert(task_id, Task::OptimizePlan(task));
+        Ok(task_id)
     }
 }
