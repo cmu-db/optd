2c453282df17e9d98fbb7e3f4f3dcb01e1730379
Alexis Schlomer <aschlome@andrew.cmu.edu>
2025-05-06 18:48:02 -0400

    Add checker for run annotation

--- optd/src/dsl/analyzer/errors.rs
+++ optd/src/dsl/analyzer/errors.rs
@@ -1,9 +1,6 @@
 use super::{hir::Identifier, type_checks::registry::Type};
 use crate::dsl::{
-    analyzer::{
-        into_hir::annotations::{IMPLEMENTATION_SIGNATURE_TYPE, TRANSFORMATION_SIGNATURE_TYPE},
-        type_checks::converter::type_display,
-    },
+    analyzer::type_checks::converter::type_display,
     utils::{
         errors::Diagnose,
         span::{Span, Spanned},
@@ -126,16 +123,11 @@ pub enum AnalyzerErrorKind {
         unknowns: HashMap<usize, Type>,
     },
 
-    InvalidTransformation {
+    InvalidAnnotation {
         span: Span,
-        actual_signature: Type,
-        // To be able to call display function of Type.
-        unknowns: HashMap<usize, Type>,
-    },
-
-    InvalidImplementation {
-        span: Span,
-        actual_signature: Type,
+        annotation_name: String,
+        actual_type: Type,
+        expected_type: Type,
         // To be able to call display function of Type.
         unknowns: HashMap<usize, Type>,
     },
@@ -318,27 +310,18 @@ impl AnalyzerErrorKind {
         .into()
     }
 
-    pub fn new_invalid_transformation(
+    pub fn new_invalid_annotation(
         span: &Span,
-        actual_signature: &Type,
+        annotation_name: &str,
+        actual_type: &Type,
+        expected_type: &Type,
         unknowns: HashMap<usize, Type>,
     ) -> Box<Self> {
-        Self::InvalidTransformation {
+        Self::InvalidAnnotation {
             span: span.clone(),
-            actual_signature: actual_signature.clone(),
-            unknowns,
-        }
-        .into()
-    }
-
-    pub fn new_invalid_implementation(
-        span: &Span,
-        actual_signature: &Type,
-        unknowns: HashMap<usize, Type>,
-    ) -> Box<Self> {
-        Self::InvalidImplementation {
-            span: span.clone(),
-            actual_signature: actual_signature.clone(),
+            annotation_name: annotation_name.to_string(),
+            actual_type: actual_type.clone(),
+            expected_type: expected_type.clone(),
             unknowns,
         }
         .into()
@@ -480,39 +463,23 @@ impl Diagnose for Box<AnalyzerError> {
                 "Only functions, maps, and arrays can be called",
             ),
             InvalidFieldAccess { object, span, field, unknowns } => self.build_invalid_field_access_report(object, span, field, unknowns),
-            InvalidTransformation {
+            InvalidAnnotation {
                 span,
-                actual_signature,
+                annotation_name,
+                actual_type,
+                expected_type,
                 unknowns,
             } => {
                 self.build_single_span_report(
                     span,
-                    "Invalid transformation function signature",
+                    &format!("Invalid '{}' function signature", annotation_name),
                     &format!(
                         "Found: '{}'",
-                        type_display(actual_signature, unknowns)
+                        type_display(actual_type, unknowns)
                     ),
                     &format!(
                         "Expected a subtype of: '{}'",
-                        type_display(&TRANSFORMATION_SIGNATURE_TYPE, unknowns)
-                    ),
-                )
-            },
-            InvalidImplementation {
-                span,
-                actual_signature,
-                unknowns,
-            } => {
-                self.build_single_span_report(
-                    span,
-                    "Invalid implementation function signature",
-                    &format!(
-                        "Found: '{}'",
-                        type_display(actual_signature, unknowns)
-                    ),
-                    &format!(
-                        "Expected a subtype of: '{}'", 
-                        type_display(&IMPLEMENTATION_SIGNATURE_TYPE, unknowns)
+                        type_display(expected_type, unknowns)
                     ),
                 )
             },
@@ -551,10 +518,9 @@ impl Diagnose for Box<AnalyzerError> {
             ArgumentNumberMismatch { span, .. } => span,
             InvalidCallReceiver { span, .. } => span,
             InvalidFieldAccess { span, .. } => span,
-            InvalidTransformation { span, .. } => span,
-            InvalidImplementation { span, .. } => span,
-            InvalidArrayDecomposition { pattern_span, .. } => pattern_span, // Use pattern span as primary
-            ReservedType { span, .. } => span, // New case for ReservedType
+            InvalidAnnotation { span, .. } => span,
+            InvalidArrayDecomposition { pattern_span, .. } => pattern_span,
+            ReservedType { span, .. } => span,
         };
 
         (span.src_file.clone(), Source::from(self.src_code.clone()))
