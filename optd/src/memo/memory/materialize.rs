570efc926ff7144e9c2622b361b7c255b6e437a0
Connor Tsui <87130162+connortsui20@users.noreply.github.com>
2025-05-14 16:49:14 -0400

    Some memo refactors (#108)
    ## Problem
    
    There were a few inefficiencies in the memo table (mainly extraneous
    clones, and how remapping of objects like goals and expressions work).
    Also, some of the function docs were quite verbose.
    
    ## Summary of changes
    
    This PR cleans up some of the memo table modules style and
    documentation, as well as removes several extraneous clones.

--- optd/src/memo/memory/materialize.rs
+++ optd/src/memo/memory/materialize.rs
@@ -2,55 +2,27 @@
 //!
 //! See the documentation for [`Materialize`] for more information.
 
-use super::{Infallible, MemoryMemo, helpers::MemoryMemoHelpers};
+use super::{Infallible, MemoryMemo, helpers::MemoryMemoHelper};
 use crate::{
     cir::*,
     memo::{Materialize, Representative},
 };
 
 impl Materialize for MemoryMemo {
-    async fn get_goal_id(&mut self, goal: &Goal) -> Result<GoalId, Infallible> {
-        // Check if the goal is already in the memo table.
-        let goal = self.remap_goal(goal).await?;
-        if let Some(&goal_id) = self.goal_to_id.get(&goal) {
-            return Ok(goal_id);
-        }
-
-        // Otherwise, create a new entry in the memo table.
-        let goal_id = self.next_goal_id();
-        self.id_to_goal.insert(goal_id, goal.clone());
-        self.goal_to_id.insert(goal.clone(), goal_id);
-
-        Ok(goal_id)
-    }
-
-    async fn materialize_goal(&self, goal_id: GoalId) -> Result<Goal, Infallible> {
-        let repr_goal_id = self.find_repr_goal_id(goal_id).await?;
-        Ok(self
-            .id_to_goal
-            .get(&repr_goal_id)
-            .unwrap_or_else(|| panic!("{:?} not found in memo table", repr_goal_id))
-            .clone())
-    }
-
     async fn get_logical_expr_id(
         &mut self,
         logical_expr: &LogicalExpression,
     ) -> Result<LogicalExpressionId, Infallible> {
         use Child::*;
 
-        // Check if the expression is already in the memo table.
+        // Check if the expression is already in the memo table (fast path).
         let remapped_expr = self.remap_logical_expr(logical_expr).await?;
-        if let Some(&expr_id) = self.logical_expr_to_id.get(&remapped_expr) {
+        if let Some(&expr_id) = self.logical_expr_to_id.get(remapped_expr.as_ref()) {
             return Ok(expr_id);
         }
 
-        // Otherwise, create a new entry in the memo table.
+        // Otherwise, create a new entry in the memo table (slow path).
         let expr_id = self.next_logical_expression_id();
-        self.id_to_logical_expr
-            .insert(expr_id, remapped_expr.clone());
-        self.logical_expr_to_id
-            .insert(remapped_expr.clone(), expr_id);
 
         // Update the logical expression to group index.
         remapped_expr
@@ -67,6 +39,12 @@ impl Materialize for MemoryMemo {
                     .insert(expr_id);
             });
 
+        // Update the logical expression ID indexes.
+        self.id_to_logical_expr
+            .insert(expr_id, remapped_expr.clone().into_owned());
+        self.logical_expr_to_id
+            .insert(remapped_expr.into_owned(), expr_id);
+
         Ok(expr_id)
     }
 
@@ -79,6 +57,30 @@ impl Materialize for MemoryMemo {
             .id_to_logical_expr
             .get(&repr_expr_id)
             .unwrap_or_else(|| panic!("{:?} not found in memo table", repr_expr_id))
+            .clone())
+    }
+
+    async fn get_goal_id(&mut self, goal: &Goal) -> Result<GoalId, Infallible> {
+        // Check if the goal is already in the memo table (fast path).
+        let goal = self.remap_goal(goal).await?;
+        if let Some(&goal_id) = self.goal_to_id.get(goal.as_ref()) {
+            return Ok(goal_id);
+        }
+
+        // Otherwise, create a new entry in the memo table (slow path).
+        let goal_id = self.next_goal_id();
+        self.id_to_goal.insert(goal_id, goal.clone().into_owned());
+        self.goal_to_id.insert(goal.into_owned(), goal_id);
+
+        Ok(goal_id)
+    }
+
+    async fn materialize_goal(&self, goal_id: GoalId) -> Result<Goal, Infallible> {
+        let repr_goal_id = self.find_repr_goal_id(goal_id).await?;
+        Ok(self
+            .id_to_goal
+            .get(&repr_goal_id)
+            .unwrap_or_else(|| panic!("{:?} not found in memo table", repr_goal_id))
             .clone())
     }
 
