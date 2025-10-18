40274ab409f87f32c3b740fd95919f8cc750651a
HFFuture <ray-huangsirui@hotmail.com>
2025-10-18 16:45:46 -0400

    Tentative statistics retriver structs and fetch_table_statistics implementation

--- optd/catalog/src/lib.rs
+++ optd/catalog/src/lib.rs
@@ -1,7 +1,9 @@
+// mod optd_catalog;
+// mod optd_table;
+mod ducklake_connection;
+mod statistics;
 
-mod optd_catalog;
-mod optd_table;
-
-pub use optd_catalog::*;
-pub use optd_table::*;
-
+// pub use optd_catalog::*;
+// pub use optd_table::*;
+pub use ducklake_connection::{ConnectionMode, DuckLakeConnectionBuilder, query};
+pub use statistics::{DuckLakeStatisticsProvider, Error as InterfaceError, StatisticsProvider};
