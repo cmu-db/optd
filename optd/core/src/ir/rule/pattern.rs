7482d36837c40526515630bb5e35d18fb843aa62
Yuchen Liang <yuchenl3@andrew.cmu.edu>
2026-01-19 10:59:59 -0500

    cargo clippy --fix
    Signed-off-by: Yuchen Liang <yuchenl3@andrew.cmu.edu>

--- optd/core/src/ir/rule/pattern.rs
+++ optd/core/src/ir/rule/pattern.rs
@@ -58,7 +58,7 @@ impl OperatorPattern {
                 .input_operator_patterns
                 .iter()
                 .all(|(i, input_pattern)| {
-                    input_ops.get(*i).map_or(false, |input_op| {
+                    input_ops.get(*i).is_some_and(|input_op| {
                         input_pattern.matches_without_expand(input_op)
                     })
                 })
