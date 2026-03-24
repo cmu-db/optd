573c58e7f7e8a306f2d482b147e1a51ce0b51944
Yuchen Liang <yuchenl3@andrew.cmu.edu>
2026-03-24 10:53:13 -0400

    add change category types
    Signed-off-by: Yuchen Liang <yuchenl3@andrew.cmu.edu>

--- optd/repository/src/entity/snapshot_changes.rs
+++ optd/repository/src/entity/snapshot_changes.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 use sea_orm::entity::prelude::*;
 
 #[sea_orm::model]
@@ -49,3 +51,53 @@ pub struct Model {
 }
 
 impl ActiveModelBehavior for ActiveModel {}
+
+#[derive(Debug, Clone, PartialEq, derive_more::Display)]
+pub enum ChangeCategory {
+    #[display("created_schema:{_0}")]
+    CreateSchema(String),
+    #[display("created_table:{_0}")]
+    CreateTable(String),
+    #[display("created_view:{_0}")]
+    CreateView(String),
+    #[display("inserted_into_table:{_0}")]
+    InsertIntoTable(i64),
+    #[display("deleted_from_table:{_0}")]
+    DeleteFromTable(i64),
+    #[display("compacted_table:{_0}")]
+    CompactTable(i64),
+    #[display("dropped_schema:{_0}")]
+    DropSchema(i64),
+    #[display("dropped_table:{_0}")]
+    DropTable(i64),
+    #[display("dropped_view:{_0}")]
+    DropView(i64),
+    #[display("altered_table:{_0}")]
+    AlterTable(i64),
+    #[display("altered_view:{_0}")]
+    AlterView(i64),
+}
+
+impl FromStr for ChangeCategory {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (first, second) = s.split_once(':').ok_or_else(|| s.to_string())?;
+        let parse_id = || second.parse::<i64>().map_err(|_| s.to_string());
+
+        match first {
+            "created_schema" => Ok(Self::CreateSchema(second.to_owned())),
+            "created_table" => Ok(Self::CreateTable(second.to_owned())),
+            "created_view" => Ok(Self::CreateView(second.to_owned())),
+            "inserted_into_table" => Ok(Self::InsertIntoTable(parse_id()?)),
+            "deleted_from_table" => Ok(Self::DeleteFromTable(parse_id()?)),
+            "compacted_table" => Ok(Self::CompactTable(parse_id()?)),
+            "dropped_schema" => Ok(Self::DropSchema(parse_id()?)),
+            "dropped_table" => Ok(Self::DropTable(parse_id()?)),
+            "dropped_view" => Ok(Self::DropView(parse_id()?)),
+            "altered_table" => Ok(Self::AlterTable(parse_id()?)),
+            "altered_view" => Ok(Self::AlterView(parse_id()?)),
+            _ => Err(s.to_string()),
+        }
+    }
+}
