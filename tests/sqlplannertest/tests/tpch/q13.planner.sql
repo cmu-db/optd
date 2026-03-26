c476c1758a86149b7250357327e6eff4de787405
Yuchen Liang <yuchenl3@andrew.cmu.edu>
2026-03-26 10:53:06 -0400

    apply planner test
    Signed-off-by: Yuchen Liang <yuchenl3@andrew.cmu.edu>

--- tests/sqlplannertest/tests/tpch/q13.planner.sql
+++ tests/sqlplannertest/tests/tpch/q13.planner.sql
@@ -40,14 +40,41 @@ OrderBy { ordering_exprs: [ __internal_#8.custdist(#8.1) DESC, __internal_#8.c_c
 
 physical_plan after optd-finalized:
 EnforcerSort { tuple_ordering: [(#8.1, Desc), (#8.0, Desc)], (.output_columns): __internal_#8.c_count(#8.0), __internal_#8.custdist(#8.1), (.cardinality): 0.00 }
-└── Project { .table_index: 8, .projections: [ c_orders.c_count(#6.1), __internal_#7.count(Int64(1))(#7.0) ], (.output_columns): __internal_#8.c_count(#8.0), __internal_#8.custdist(#8.1), (.cardinality): 0.00 }
-    └── Aggregate { .aggregate_table_index: 7, .implementation: None, .exprs: [ count(1::bigint) ], .keys: [ c_orders.c_count(#6.1) ], (.output_columns): __internal_#7.count(Int64(1))(#7.0), c_orders.c_count(#6.1), (.cardinality): 0.00 }
+└── Project
+    ├── .table_index: 8
+    ├── .projections: [ c_orders.c_count(#6.1), __internal_#7.count(Int64(1))(#7.0) ]
+    ├── (.output_columns): __internal_#8.c_count(#8.0), __internal_#8.custdist(#8.1)
+    ├── (.cardinality): 0.00
+    └── Aggregate
+        ├── .aggregate_table_index: 7
+        ├── .implementation: None
+        ├── .exprs: [ count(1::bigint) ]
+        ├── .keys: [ c_orders.c_count(#6.1) ]
+        ├── (.output_columns): __internal_#7.count(Int64(1))(#7.0), c_orders.c_count(#6.1)
+        ├── (.cardinality): 0.00
         └── Remap { .table_index: 6, (.output_columns): c_orders.c_count(#6.1), c_orders.c_custkey(#6.0), (.cardinality): 0.00 }
-            └── Project { .table_index: 5, .projections: [ __internal_#4.c_custkey(#4.0), __internal_#4.count(orders.o_orderkey)(#4.1) ], (.output_columns): __internal_#5.c_count(#5.1), __internal_#5.c_custkey(#5.0), (.cardinality): 0.00 }
-                └── Project { .table_index: 4, .projections: [ customer.c_custkey(#1.0), __internal_#3.count(orders.o_orderkey)(#3.0) ], (.output_columns): __internal_#4.c_custkey(#4.0), __internal_#4.count(orders.o_orderkey)(#4.1), (.cardinality): 0.00 }
-                    └── Aggregate { .aggregate_table_index: 3, .implementation: None, .exprs: [ count(orders.o_orderkey(#2.0)) ], .keys: [ customer.c_custkey(#1.0) ], (.output_columns): __internal_#3.count(orders.o_orderkey)(#3.0), customer.c_custkey(#1.0), (.cardinality): 0.00 }
-                        └── Join
-                            ├── .join_type: Left
+            └── Project
+                ├── .table_index: 5
+                ├── .projections: [ customer.c_custkey(#1.0), __internal_#3.count(orders.o_orderkey)(#3.0) ]
+                ├── (.output_columns): __internal_#5.c_count(#5.1), __internal_#5.c_custkey(#5.0)
+                ├── (.cardinality): 0.00
+                └── Aggregate
+                    ├── .aggregate_table_index: 3
+                    ├── .implementation: None
+                    ├── .exprs: [ count(orders.o_orderkey(#2.0)) ]
+                    ├── .keys: [ customer.c_custkey(#1.0) ]
+                    ├── (.output_columns): __internal_#3.count(orders.o_orderkey)(#3.0), customer.c_custkey(#1.0)
+                    ├── (.cardinality): 0.00
+                    └── Join
+                        ├── .join_type: Left
+                        ├── .implementation: None
+                        ├── .join_cond: (customer.c_custkey(#1.0) = orders.o_custkey(#2.1)) AND (orders.o_comment(#2.8) NOT LIKE CAST (%special%requests%::utf8 AS Utf8View))
+                        ├── (.output_columns): customer.c_custkey(#1.0), orders.o_comment(#2.8), orders.o_custkey(#2.1), orders.o_orderkey(#2.0)
+                        ├── (.cardinality): 0.00
+                        ├── Get { .data_source_id: 6, .table_index: 1, .implementation: None, (.output_columns): customer.c_custkey(#1.0), (.cardinality): 0.00 }
+                        └── Get
+                            ├── .data_source_id: 7
+                            ├── .table_index: 2
                             ├── .implementation: None
                             ├── (.output_columns): orders.o_comment(#2.8), orders.o_custkey(#2.1), orders.o_orderkey(#2.0)
                             └── (.cardinality): 0.00
