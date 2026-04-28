f01fd626a1d7c35e3cd34274bccaef38a2b166a3
Yuchen Liang <yuchenl3@andrew.cmu.edu>
2026-04-28 10:23:34 -0400

    move profile extension to a separate module
    Signed-off-by: Yuchen Liang <yuchenl3@andrew.cmu.edu>

--- optd/core/src/rules/mod.rs
+++ optd/core/src/rules/mod.rs
@@ -6,6 +6,7 @@ mod logical_join_inner_commute;
 mod logical_select_join_transpose;
 mod logical_select_simplify;
 mod pass;
+mod profile;
 mod simplification;
 
 pub use decorrelation::*;
@@ -15,5 +16,6 @@ pub use logical_join_inner_assoc::LogicalJoinInnerAssocRule;
 pub use logical_join_inner_commute::LogicalJoinInnerCommuteRule;
 pub use logical_select_join_transpose::LogicalSelectJoinTransposeRule;
 pub use logical_select_simplify::LogicalSelectSimplifyRule;
-pub use pass::{PassExtension, PassManager, PassManagerBuilder, PassProfilingExtension, PlanPass};
+pub use pass::{PassExtension, PassManager, PassManagerBuilder, PlanPass};
+pub use profile::PassProfilingExtension;
 pub use simplification::SimplificationPass;
